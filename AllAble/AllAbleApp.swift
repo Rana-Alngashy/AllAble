bc06f48f0f077fcd303d8ff2b4b46f6c9f3f004d
Rana <rana.k.alngashy@gmail.com>
2025-11-30 14:52:10 +0300

    Initial Commit

---
+++ AllAble/AllAbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllAbleApp.swift
+//  AllAble
+//
+//  Created by Rana Alngashy on 09/06/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct AllAbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
